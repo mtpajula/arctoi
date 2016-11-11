c75eb1b8b0964fd27b327915f49ee3a5e1f92030
Mikko Pajula <mtpajula@gmail.com>
2016-11-11 13:23:23 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # arctoi
+
 Mapping platform
+
+# Ohjelmasta
+
+Modulaarinen javascript-karttaohjelma, jolla voi käsitellä mittausdataa.
+
+Lisenssi: GNU GENERAL PUBLIC LICENSE, Version 3
+
+# Credits
+
+bootstrap
+Code licensed MIT, docs CC BY 3.0.
+
+jQuery
+Copyright jQuery Foundation and other contributors
+
+Leaflet
+Copyright (c) 2010-2016, Vladimir Agafonkin
+Copyright (c) 2010-2011, CloudMade
+All rights reserved.
+
+Proj4js
+https://github.com/proj4js/proj4js/blob/master/LICENSE.md
---
+++ js/arctoi-map.js
@@ -0,0 +1,20 @@
+var map = L.map('map');
+
+var openstreetmap = new L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+});
+
+
+var peruskartta = new L.TileLayer('http://{s}.kartat.kapsi.fi/peruskartta/{z}/{x}/{y}.png', {
+    attribution: 'Kartta: Maanmittauslaitos',
+    maxZoom: 18,
+    subdomains: ['tile1', 'tile2']
+});
+var ortokuva = new L.TileLayer('http://{s}.kartat.kapsi.fi/ortokuva/{z}/{x}/{y}.png', {
+    attribution: 'Kartta: Maanmittauslaitos',
+    maxZoom: 18,
+    subdomains: ['tile1', 'tile2']
+});
+
+L.control.scale().addTo(map);
+map.setView(new L.LatLng(60.1708, 24.9375), 7).addLayer(openstreetmap);
