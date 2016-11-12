911f2295e5487eeba0951a41fa10c734ce0fb7f9
Mikko Pajula <mtpajula@gmail.com>
2016-11-12 10:50:36 +0200

    demo-ui build

--- README.md
+++ README.md
@@ -2,24 +2,22 @@
 
 Mapping platform
 
-# Ohjelmasta
+## Ohjelmasta
 
 Modulaarinen javascript-karttaohjelma, jolla voi käsitellä mittausdataa.
 
-Lisenssi: GNU GENERAL PUBLIC LICENSE, Version 3
+## Credits
 
-# Credits
-
-bootstrap
+[bootstrap](http://getbootstrap.com/)
 Code licensed MIT, docs CC BY 3.0.
 
-jQuery
+[jQuery](http://getbootstrap.com/)
 Copyright jQuery Foundation and other contributors
 
-Leaflet
+[Leaflet](http://getbootstrap.com/)
 Copyright (c) 2010-2016, Vladimir Agafonkin
 Copyright (c) 2010-2011, CloudMade
 All rights reserved.
 
-Proj4js
+[Proj4js](http://getbootstrap.com/)
 https://github.com/proj4js/proj4js/blob/master/LICENSE.md
--- js/arctoi-map.js
+++ js/arctoi-map.js
@@ -4,7 +4,6 @@ var openstreetmap = new L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 });
 
-
 var peruskartta = new L.TileLayer('http://{s}.kartat.kapsi.fi/peruskartta/{z}/{x}/{y}.png', {
     attribution: 'Kartta: Maanmittauslaitos',
     maxZoom: 18,
@@ -16,5 +15,27 @@ var ortokuva = new L.TileLayer('http://{s}.kartat.kapsi.fi/ortokuva/{z}/{x}/{y}.
     subdomains: ['tile1', 'tile2']
 });
 
+var positions = new L.FeatureGroup();
+var points = new L.FeatureGroup();
+var rightangle = new L.FeatureGroup();
+
 L.control.scale().addTo(map);
 map.setView(new L.LatLng(60.1708, 24.9375), 7).addLayer(openstreetmap);
+
+var baseMaps = {
+    'OpenStreetMap': openstreetmap,
+    'Peruskartta': peruskartta,
+    'Ilmakuva': ortokuva,
+};
+
+var overlayMaps = {
+    'Havainnot' : positions,
+    'Pisteet' : points,
+    'Suorakulmainen mittaus' : rightangle
+};
+
+map.addLayer(positions);
+map.addLayer(points);
+map.addLayer(rightangle);
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
